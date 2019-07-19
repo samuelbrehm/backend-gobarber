08339beb10e4a032777e6f4172a1c3386ca3395f
Samuel Brehm <samuelbrehm@gmail.com>
2019-07-19 11:17:16 -0300

    Created loader models

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello Samuel!' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Samuel Brehm',
+    email: 'samuelbrehm@gmail.com',
+    password_hash: '123456',
+  });
+
+  return res.json(user);
 });
 
 export default routes;

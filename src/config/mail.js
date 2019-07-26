e0650b0f4c80413e59e8f618b1093bc47a91f7ba
Samuel Brehm <samuelbrehm@gmail.com>
2019-07-26 10:09:38 -0300

    Settings mail service

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'c4a4fd5a93d8f5',
+    pass: '2c9f502fa16ead',
+  },
+  default: {
+    from: 'Equipe Gobarber <noreply@gobarber.com>',
+  },
+};
